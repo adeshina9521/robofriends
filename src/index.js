dd5e95f4ec27f7fd3b48215c311dc9301f42a2ea
adeshina9521 <abdullateef.adegoke@gmail.com>
2020-04-03 16:44:34 +0100

    to connect action,constant and reducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 //import Card from './Card'; // or (./Card.js)
 import * as serviceWorker from './serviceWorker';
@@ -7,9 +9,15 @@ import 'tachyons';
 //import { robots } from './robot';
 // import CardList from './CardList';
 import App from './Containers/App';
+import { searchRobots } from './reducer';
+
+const store = createStore(searchRobots)
 
 
-ReactDOM.render( <App/>, document.getElementById('root'));
+ReactDOM.render(<Provider store = {store}>
+                    <App />
+                </Provider>
+             , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
